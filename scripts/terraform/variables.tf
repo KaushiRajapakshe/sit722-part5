6feb6d03c567fa1db632c8205ba03c121dcfdbbe
Kaushalya Rajapaksha <kaushi.rajapakshe1@gmail.com>
2024-09-21 09:24:29 +1000

    Added initial development

---
+++ scripts/terraform/variables.tf
@@ -0,0 +1,13 @@
+# Sets global variables for this Terraform project.
+
+variable app_name {
+  default = "sit722week09task05D"
+}
+
+variable location {
+  default = "australiaeast"
+}
+
+variable kubernetes_version {    
+  default = "1.30.3"
+}
